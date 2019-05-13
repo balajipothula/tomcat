e3f8646ff791d125bd1142104f530b05f0224d9e
BALAJI POTHULA <balan.pothula@gmail.com>
2019-05-13 16:58:47 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -45,3 +45,6 @@ docker pull balajipothula/jenkins:latest
 # running docker container with name(--name) "jenkins" as daemon(-d),
 # stdin(-i) with volume(-v) "tomcat" on port(-p) "8080".
 docker run --name jenkins -d -i -p 8080:8080 --privileged -v $HOME/jenkins/tomcat:/tomcat balajipothula/jenkins:latest sh
+
+# executing docker container by name with stdin(-i), starting tomcat server.
+docker exec -i jenkins /tomcat/bin/startup.sh
