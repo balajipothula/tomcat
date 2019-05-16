e34488bb59c8b18e46da26393cbaccdc69b1865a
BALAJI POTHULA <balan.pothula@gmail.com>
2019-05-16 15:25:01 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,9 +6,6 @@
 
 # Note: Please run this script with sudo privilage.
 
-# setting maven version.
-readonly MVN_VER=3.6.1
-
 # updating packages index.
 apt update
 
@@ -16,15 +13,7 @@ apt update
 DEBIAN_FRONTEND=noninteractive apt -y upgrade
 
 # installing openjdk8.
-# maven3.3+ require jdk1.7+
 apt install -y openjdk-8-jdk
-
-cd $HOME                                                                                                         \
-  && wget http://mirrors.estointernet.in/apache/maven/maven-3/$MVN_VER/binaries/apache-maven-$MVN_VER-bin.tar.gz \
-  && tar xzf apache-maven-$MVN_VER-bin.tar.gz                                                                    \
-  && mv apache-maven-$MVN_VER $HOME/maven                                                                        \
-  && ln -s $HOME/maven/bin/mvn /usr/bin/mvn                                                                      \
-  && rm apache-maven-$MVN_VER-bin.tar.gz
 
 # extracting tomcat tar ball and removing.
 tar xzf $HOME/jenkins/tomcat.tar.gz -C $HOME/jenkins && rm $HOME/jenkins/tomcat.tar.gz
@@ -32,5 +21,5 @@ tar xzf $HOME/jenkins/tomcat.tar.gz -C $HOME/jenkins && rm $HOME/jenkins/tomcat.
 # downloading jenkins.war
 wget https://updates.jenkins-ci.org/latest/jenkins.war -O $HOME/jenkins/tomcat/webapps/ROOT.war
 
-# starting tomcat server
+# starting tomcat server.
 sh $HOME/jenkins/tomcat/bin/startup.sh
