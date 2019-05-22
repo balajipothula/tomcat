03afa8367a5f106d0cbabe98a28999c2f2ef7ebc
BALAJI POTHULA <balan.pothula@gmail.com>
2019-05-22 13:35:24 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,22 +4,14 @@
 # Date        : 09 May 2019,
 # Description : Installing openjdk, tomcat, jenkins on Ubuntu.
 
-# Note: Please run this script with sudo privilage.
-
 # updating packages index.
 apt update
 
 # upgrading packages.
-DEBIAN_FRONTEND=noninteractive apt -y upgrade
+sudo DEBIAN_FRONTEND=noninteractive apt -y upgrade
 
 # installing openjdk8.
-apt install -y openjdk-8-jre
-
-# downloading jenkins.war
-wget https://updates.jenkins-ci.org/latest/jenkins.war -O $HOME/tomcat/webapps/ROOT.war
+sudo apt install -y openjdk-8-jre
 
 # starting tomcat server.
 sh $HOME/tomcat/bin/startup.sh
-
-# jenkins unlock initial admin password.
-##sudo cat /root/.jenkins/secrets/initialAdminPassword
